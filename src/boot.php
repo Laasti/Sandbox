6a97547c61f37b3ab246931b5bb835e41ccc3001
nebulousGirl <contact@nebulousweb.com>
2015-06-09 22:04:22 -0400

    Cleaned up mess in boot to move to application class

--- src/boot.php
+++ src/boot.php
@@ -3,19 +3,13 @@
 define('BASEPATH', __DIR__);
 
 // include the Composer autoloader
-require '../vendor/autoload.php';
+require BASEPATH.'/../vendor/autoload.php';
+
+$app = new Laasti\Application();
 
 //Instantiate application services
-$di = new League\Container\Container();
-$stack = new Laasti\Stack\ContainerStack($di);
-$route = new League\Route\RouteCollection($di);
-$router = new Laasti\Route\RouteCollector('', $route, $di);
-$route->setStrategy($di->get('Laasti\Route\Strategies\RouteStrategy'));
-//Add dependencies
-$di->add('League\Route\RouteCollection', $route);
-$di->add('Laasti\Stack\StackInterface', $stack);
-$di->add('Laasti\Stack\Stack', $stack);
-$di->add('Symfony\Component\HttpFoundation\Request', Symfony\Component\HttpFoundation\Request::createFromGlobals());
+$stack = $app->getStack();
+$router = $app->getRouter();
 
 //Prepare middleware stack
 $stack->push('Laasti\Route\Middlewares\RouteMiddleware');
@@ -26,10 +20,4 @@ $router->create('GET', '/', 'Laasti\\Controllers\\HelloWorld::output');
 $router->create('GET', '/hello/{name:word}', 'Laasti\\Controllers\\HelloWorld::hello');
 
 //Start the application
-$request = $di->get('Symfony\Component\HttpFoundation\Request');
-
-
-$response = $stack->execute($request);
-$response->send();
-
-$stack->close($request, $response);
+$app->run();
